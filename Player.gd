ab3c8b81e5faf3edea43fe6e9c21f540ea840ada
UnrealisticDino <jdzubieta@gmail.com>
2023-07-29 09:47:17 -0500

    player takes damage and game over

--- Player.gd
+++ Player.gd
@@ -1,11 +1,14 @@
-extends KinematicBody2D
+extends Area2D
+
 var experience = 0  # The player's experience points
 var speed = 200  # Speed of the player
 var Orb = preload("res://orb1.tscn")  # Load the orb scene
 var level = 1  # The player's level
+var health = 100  # The player's health
 
 func _ready():
 	position = get_viewport_rect().size / 2
+	connect("area_entered", self, "_on_Player_area_entered")  # Connect the area_entered signal
 
 func _physics_process(delta):
 	var velocity = Vector2()  # The player's movement vector
@@ -22,7 +25,7 @@ func _physics_process(delta):
 
 	velocity = velocity.normalized() * speed  # Normalize the vector for consistent speed
 
-	move_and_slide(velocity)  # Move the player
+	position += velocity * delta  # Move the player
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
@@ -31,14 +34,25 @@ func _input(event):
 		orb.position = position  # Set the orb's position to the player's position
 		var direction = get_global_mouse_position() - position  # Get the direction to the mouse
 		orb.shoot(direction.normalized())  # Shoot the orb in the direction of the mouse
-		
+
+		# Connect the hit signal from the orb to all monsters
+		for monster in get_tree().get_nodes_in_group("monsters"):
+			orb.connect("hit", monster, "_on_Monster_hit")
+
 func gain_experience(amount):
 	experience += amount
 	if experience >= 20:
 		level_up()  # Level up the player
-		
+
 # This function is called when the player levels up
 func level_up():
 	level += 1
 	experience = 0  # Reset the experience points
 	print("Leveled up! Current level: " + str(level))
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("monsters"):
+		health -= 10  # Reduce the player's health by 10
+		if health <= 0:
+			print("Game over!")  # If the player's health is 0 or less, end the game
+			get_tree().quit()  # Stop the game
